acaa47200d92e7dbd0a1209c708864f055e87a2b
Gil <gil.rodrigues286@gmail.com>
2022-04-28 20:27:22 +0100

    Errors control WIP

--- src/routes/UsersRouter.ts
+++ src/routes/UsersRouter.ts
@@ -2,6 +2,10 @@ import { Router, Request, Response } from 'express';
 
 const usersRouter = Router();
 
+usersRouter.get('/', (req: Request, res: Response) => {
+  return res.json({ message: 'Right here' });
+});
+
 usersRouter.post('/', (req: Request, res: Response) => {
   return res.json({ message: 'Right here' });
 });
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { errors } from 'celebrate';
 import router from './routes';
+import 'express-async-errors';
 import 'reflect-metadata';
+
+import AppError from './models/AppError';
 
 import './datasource';
 
@@ -8,6 +12,24 @@ const app = express();
 app.use(express.json());
 
 app.use('/', router);
+
+app.use(errors());
+
+app.use((err: Error, req: Request, res: Response) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message
+    });
+  }
+
+  console.error(err);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  });
+});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
